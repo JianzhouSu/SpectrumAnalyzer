f6271b78e9938737a88df54f36329bf9db81b027
JianzhouSu-lab <sujianzhou@outlook.com>
2020-03-16 12:09:13 -0400

    class 4294A debugged

--- @A4294A/A4294A.m
+++ @A4294A/A4294A.m
@@ -2,6 +2,7 @@ classdef A4294A
 
     properties
         serialPort; % serial port class
+        par;
     end
 
     methods
@@ -9,12 +10,15 @@ classdef A4294A
         function obj = A4294A(comPort)
             % A4294A Network analyzer class
 
+            % reset all instruments connected to matlab.
+            instrreset;
+%             obj.par = parameter_setup();
             % According to prologix, baud rate doesn't matters.
             % Sets terminator of message is LF(ascii 10)
             obj.serialPort = serial(comPort, 'BaudRate', 115200, 'Terminator', 'LF');
 
             % Communication timeout setup (unit: seconds)
-            obj.serialPort.Timeout = 15;
+            obj.serialPort.Timeout = 5;
             % Input buffer size setup
             obj.serialPort.InputBufferSize = 10 * 20001;
             % Initialize
@@ -40,7 +44,7 @@ classdef A4294A
         end
 
         function output = getId(obj)
-            obj.write("IDN?");
+            obj.write('*IDN?');
             idn = obj.read();
             disp(['Serial ready:',idn]);
             output = idn;
@@ -51,15 +55,15 @@ classdef A4294A
         % Initialize prologix and Agilent 4924A
 
             fopen(obj.serialPort);
-            obj.write("++mode 1");
-            obj.write("++addr 17");
-            obj.write("++auto 1");
-            obj.write("++eoi 1");
-            obj.write("++eos 2");
-            obj.write("read_tmo_ms 3000");
+            obj.write('++mode 1');
+            obj.write('++addr 17');
+            obj.write('++auto 1');
+            obj.write('++eoi 1');
+            obj.write('++eos 2');
+            obj.write('read_tmo_ms 3000');
             obj.getId();
 
-            obj.wait();
+%             obj.wait();
 
         end
 
