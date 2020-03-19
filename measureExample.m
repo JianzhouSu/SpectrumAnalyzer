4fb840166fa98756beec95a3047c18c12e451001
JianzhouSu-lab <sujianzhou@outlook.com>
2020-03-19 14:45:50 -0400

    add instrreset in example

--- measureExample.m
+++ measureExample.m
@@ -2,6 +2,7 @@ function measureExample()
 %MEASUREEXAMPLE Summary of this function goes here
 %   Detailed explanation goes here
     clear;
+    instrreset;
 %     dbstop if error;
     m = measurement();
     dataAnalyzerExample(m.parameters.dir);
