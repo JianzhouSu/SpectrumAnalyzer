0434f473839dc2fb7ec8633590b9a738fc012476
JianzhouSu <su.jian@kusky.neu.edu>
2020-03-19 16:31:10 -0400

    small change

--- measureExample.m
+++ measureExample.m
@@ -1,9 +1,8 @@
 function measureExample()
-%MEASUREEXAMPLE Summary of this function goes here
+%measureExample Summary of this function goes here
 %   Detailed explanation goes here
     clear;
     instrreset;
-%     dbstop if error;
     m = measurement();
     dataAnalyzerExample(m.parameters.dir);
 end
