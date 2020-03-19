6b3d90ff93ffaedd68f5a09c69e22d085bcbb047
JianzhouSu <su.jian@kusky.neu.edu>
2020-03-19 09:46:52 -0400

    document A4294A

--- @A4294A/A4294A.m
+++ @A4294A/A4294A.m
@@ -1,52 +1,71 @@
 classdef A4294A < handle
+    % A4294A class for network analyzer Agilent A4294A
+    %   property: SERIALPORT serial class of device. Treated as device.
 
     properties
+        % all properties are public, should be improved in future.
         serialPort; % serial port class
-
     end
 
     methods
 
         function obj = A4294A(comPort)
-            % A4294A Network analyzer class
+            % A4294A Network analyzer class constructor
 
             % reset all instruments connected to matlab.
-            instrreset;
+            % Might create some problem when multiple devices connected.
+            % instrreset;
 
             % According to prologix, baud rate doesn't matters.
             % Sets terminator of message is LF(ascii 10)
             obj.serialPort = serial(comPort, 'BaudRate', 115200, 'Terminator', 'LF');
 
             % Communication timeout setup (unit: seconds)
-            obj.serialPort.Timeout = 5;
-            % Input buffer size setup
+            obj.serialPort.Timeout = 15;
+
+            % Input buffer size: make it large enough to save one sweep.
             obj.serialPort.InputBufferSize = 10 * 20001;
-            % Initialize
+
+            % Initialize device
             obj.init();
         end
 
+        function delete(obj)
+            % Destructor
+            %   function processed when instance is cleared.
+
+            % close serial port
+            fclose(obj.serialPort);
+        end
+
         function write(obj, command)
+            % WRITE Write command on serial port.
+            %   input: command should be strings
             fprintf(obj.serialPort, command);
         end
 
         function output = read(obj)
+            % READ Read the Ascii string from serial port.
+            %   Return strings from serial port.
             output = fgets(obj.serialPort);
         end
 
         function stats = wait(obj)
-            % todo: need a better wait program
+            % todo: haven't done yet.
             pause(0.1);
-%             obj.write("*OPC?");
-% 
-%             if isempty(obj.read())
-                stats = 'Ready';
-%             else
-%                 stats = 'Busy';
-%             end
+            %             obj.write("*OPC?");
+            %
+            %             if isempty(obj.read())
+            stats = 'Ready';
+            %             else
+            %                 stats = 'Busy';
+            %             end
 
         end
 
         function output = getId(obj)
+            % GetID query ID information from device.
+            %   used as a function to test communication between device and PC.
             obj.write('*IDN?');
             idn = obj.read();
             disp(['Serial ready:', idn]);
@@ -58,27 +77,32 @@ classdef A4294A < handle
             % Initialize prologix and Agilent 4924A
 
             fopen(obj.serialPort);
+            % prologix (serial to gpib converter) settings
             obj.write('++mode 1');
             obj.write('++addr 17');
             obj.write('++auto 1');
             obj.write('++eoi 1');
             obj.write('++eos 2');
             obj.write('read_tmo_ms 3000');
+            % Test serial communication
             obj.getId();
-
+            % Wait when device is occupied, need to be improved.
             obj.wait();
 
         end
 
         function [data1, data2] = oneSweep(obj)
-            %myFun - Description
+            %oneSweep - Return one sweep data
             %
             % Syntax: data1, data2 = oneSweep(obj)
             %
-            % Long description
+            % Query device for one sweep data. When using data format 'RX', data1 should be real part and data2 is imagine part.
+            % Base on code for 4294A by Vineeth.
+
             obj.write('HOLD');
             obj.write('TRGS INT');
             obj.write('SING');
+            % todo: need to be improved, 10 seconds delay might be too much.
             pause(10);
             disp(obj.wait());
             pause(0.1);
